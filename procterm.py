69503adf3eff7f7a8586dd6d25b841d3e097d04f
jeremy <j@e.com>
2021-11-03 12:09:30 -0700

    Added dryrun

--- procterm.py
+++ procterm.py
@@ -3,6 +3,8 @@ import logging
 import subprocess
 import re
 from email.mime.text import MIMEText
+import socket
+import os,pwd
 haserror = False
 
 def main():
@@ -10,13 +12,15 @@ def main():
     'p4 monitor kill' to mark it for termination."""
     p = argparse.ArgumentParser(description="Marks long running processes for termination.")
     p.add_argument('--p4bin',dest='p4bin', default='/opt/icmanage/bin/p4',help='Filepath to P4 binary.')
-    p.add_argument('-l', dest='hourlim', default=240, help='Max number of hours a process can run before being marked for termination.',
-                   type=int)
+    p.add_argument('-l', dest='hourlim', default=240,
+                   help='Max number of hours a process can run before being marked for termination.', type=int)
     p.add_argument('-p', dest='p4port', required=True, help='P4PORT for main server.')
     p.add_argument('-u', dest='p4user', required=False, default='icmAdmin', help='P4 super user.')
     p.add_argument('-m', dest='emails', default="jeremy.yung@icmanage.com",
                    help='Email recipient list.(a@email.com,b@email.com)')
     p.add_argument('--debug', dest='debug', default=False, action='store_true', help='Turn on debug output.')
+    p.add_argument('--dryrun', dest='dryrun', default=False, action='store_true',
+                   help='Do a dryrun without terminating processes.')
     args = p.parse_args()
 
     #splitMon(" 2282 I icmAdmin   95:04:52 IDLE ")
@@ -26,26 +30,35 @@ def main():
 
     stdout,stderr,errorcode = runMonShow(p4_cmd_prefix)
     if errorcode > 0:
-        logging.error("P4 Error: \n %s" % stderr)
+        errorstr = "P4 Error: \n %s" % stderr
+        logging.error(errorstr)
+        mailReport([errorstr],args.emails,args.hourlim)
     else:
         logging.debug(stdout)
         allprocs = stdout.split('\n')
+        termlist = []
         for proc in allprocs:
             if proc != '':
-                PID,status,hours = splitMon(proc)
+                PID,hours,procstr = splitMon(proc)
                 if hours > args.hourlim:
-                    logging.error("PID(%s): %s hour runtime exceeds %s hour limit." % (PID,hours,args.hourlim))
-                    runMonTerm(p4_cmd_prefix, PID)
+                    termlist.append(procstr)
+                    if not args.dryrun:
+                        logging.error("Terminating %s, runtime %s hours" % (PID,hours))
+                        runMonTerm(p4_cmd_prefix, PID)
+                    else:
+                        logging.error("Dryrun: %s runtime is %s hours" % (PID, hours))
+        if len(termlist) > 0 and not args.dryrun:
+            mailReport(termlist,args.emails,args.hourlim)
     exit(0)
 
-def splitMon(line):
-    PID = re.search('^.?\d+',line).group().strip()
-    status = re.search(' \w ', line).group().strip()
-    hours = int(re.search(' \d+(?=:)', line).group().strip())
-    return(PID,status,hours)
+def splitMon(procstr):
+    PID = re.search('^.?\d+',procstr).group().strip()
+    #status = re.search(' \w ', line).group().strip()
+    hours = int(re.search(' \d+(?=:)', procstr).group().strip())
+    return(PID,hours,procstr)
 
 def runMonShow(p4prefix):
-    cmdstr = "%s monitor show" % p4prefix
+    cmdstr = "%s monitor show -a" % p4prefix
     return runCMD(cmdstr)
 
 def runMonTerm(p4prefix,PID):
@@ -60,6 +73,23 @@ def runCMD(cmdstr):
     stderr = result[1].decode("utf-8")
     return_code = process.returncode
     return (stdout,stderr,return_code)
+
+def mailReport(termlist, emails, hourlimit):
+    logging.debug("Error(s) encountered, sending email notice to %s." % emails)
+    report = '\n'.join(termlist)
+    report += "\nNOTE: This email will be sent if an error was encountered as well.\n"
+    report += "\nSent by\n"
+    report += "Host:    %s\n" % socket.gethostname()
+    report += "User:    %s\n" % pwd.getpwuid(os.getuid())[0]
+    report += "Program: %s\n" % os.path.basename(__file__)
+    logging.debug('Email message:\n' + report)
+
+    msg = MIMEText(report)
+    msg["To"] = emails
+    msg["Subject"] = "Perforce processes running longer than %s hours" % hourlimit
+    msg["From"] = "icmanage@xilinx.com"
+    p = subprocess.Popen(["/usr/sbin/sendmail", "-t"], stdin=subprocess.PIPE)
+    p.communicate(msg.as_bytes())
 
 if __name__ == '__main__':
     main()
